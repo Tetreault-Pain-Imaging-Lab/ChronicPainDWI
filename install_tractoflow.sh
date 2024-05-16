c42ba0218c2a27d4c394d78494ce0cb34dbaeb52
Ludoalevesque <ludo.a.levesque@gmail.com>
2024-05-16 10:09:54 -0700

    adding scripts

---
+++ install_tractoflow.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Help message function
+display_help() {
+    echo "Usage: $0 [directory]"
+    echo "Installs TractoFlow in the specified directory."
+    echo "Arguments:"
+    echo "  directory   The directory where TractoFlow will be installed."
+}
+
+# Check for the --help option
+if [[ "$1" == "--help" ]]; then
+    display_help
+    exit 0
+fi
+
+# Check for the number of command-line arguments
+if [[ $# -eq 0 ]]; then
+    echo "Error: Not enough arguments. Provide the directory where you want to install TractoFlow."
+    exit 1
+elif [[ $# -gt 1 ]]; then
+    echo "Error: Too many arguments."
+    exit 1
+fi
+
+# Initialize a variable 'directory' with the value of the argument
+directory="$1"
+
+# Load the required module
+module load StdEnv/2020 java/14.0.2 nextflow/22.10.8 apptainer/1.1.8
+
+# Clone the TractoFlow repository
+cd "$directory" || exit 1
+git clone https://github.com/scilus/tractoflow.git || exit 1
+
+# Build the .sif file in a directory called containers
+mkdir containers
+wget https://scil.usherbrooke.ca/containers/scilus_1.6.0.sif || exit 1
+
---
+++ run_tractoflow_cc.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This would run Tractoflow with the following parameters:
+#   - bids: clinical data from TPIL lab (27 CLBP and 25 control subjects), if not in BIDS format use flag --input
+#   - with-singularity: container image scilus v1.4.0 (runs: dmriqc_flow, tractoflow, recobundleX, tractometry)
+#   - with-report: outputs a processing report when pipeline is finished running
+#   - Dti_shells 0 and 1000 (usually <1200), Fodf_shells 0 1000 and 2000 (usually >700, multishell CSD-ms).
+#   - profile: bundling, bundling profile will set the seeding strategy to WM as opposed to interface seeding that is usually used for connectomics
+
+
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=0
+#SBATCH --time=168:00:00
+
+#SBATCH --mail-user=ludo.a.levesque@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+
+
+module load StdEnv/2020 java/14.0.2 nextflow/22.10.8 apptainer/1.1.8
+
+my_singularity_img='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/containers/scilus_1.6.0.sif' # or .img
+my_main_nf='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_scil/tractoflow/main.nf'
+my_input='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/BIDS_dataset_longitudinale/dataset_v2/'
+my_bidsignore='/home/pabaua/projects/def-pascalt-ab/pabaua/dev_tpil/data/BIDS_dataset_longitudinale/.bidsignore_tractoflow'
+
+nextflow run $my_main_nf --bids $my_input \
+    -with-singularity $my_singularity_img -resume -with-report report.html \
+    --dti_shells "0 1000" --fodf_shells "0 1000 2000" -profile bundling --run_gibbs_correction true \
+    --bidsignore $my_bidsignore
+s
