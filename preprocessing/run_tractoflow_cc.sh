7400ec2d825c68e43ef3318a5607e7e117a16af6
Ludoalevesque <ludo.a.levesque@gmail.com>
2024-05-23 17:38:31 -0400

    changed the time limit on run_tractoflow_cc to 30h

--- preprocessing/run_tractoflow_cc.sh
+++ preprocessing/run_tractoflow_cc.sh
@@ -16,7 +16,7 @@
                                # https://docs.computecanada.ca/wiki/B%C3%A9luga/en#Node_Characteristics
 #SBATCH --mem=0                # --> 0 means you take all the memory of the node. If you think you will need
                                # all the node, you can keep 0.
-#SBATCH --time=50:00:00
+#SBATCH --time=30:00:00
 
 #SBATCH --mail-user=ludo.a.levesque@gmail.com
 #SBATCH --mail-type=BEGIN
