4db16eff5af93e7ef44a8f9bfffe8f6de1218c9b
Ludovic Arsenault-Lévesque <122315006+Ludoalevesque@users.noreply.github.com>
2024-06-17 15:20:12 -0400

    Update run_qc_tractoflow_cc.sh
    Signed-off-by: Ludovic Arsenault-Lévesque <122315006+Ludoalevesque@users.noreply.github.com>

--- qc/run_qc_tractoflow_cc.sh
+++ qc/run_qc_tractoflow_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This would run dmriqc_flow with the following parameters:
-#   - 
+#   -profile tractoflow_qc_all : Create QC reports for your TractoFlow results and outputs all file
 
 
 #SBATCH --nodes=1              # --> Generally depends on your nb of subjects.
@@ -9,21 +9,13 @@
                                # that if you have more subjects than cores/cpus (ex, if you process 38
                                # subjects on 32 cpus-per-task), you could ask for one more node.
 #SBATCH --cpus-per-task=10     # --> You can see here the choices. For beluga, you can choose 32, 40 or 64.
-                               # https://docs.computecanada.ca/wiki/B%C3%A9luga/en#Node_Characteristics
+                               # https://docs.computecanada.ca/wiki/B%C3%A9luga/en
+                               #Node_Characteristics
 #SBATCH --mem=0                # --> 0 means you take all the memory of the node. If you think you will need
                                # all the node, you can keep 0.
 #SBATCH --time=10:00:00
 
-#SBATCH --mail-user=ludo.a.levesque@gmail.com
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-type=REQUEUE
-#SBATCH --mail-type=ALL
-#SBATCH --output="/home/ludoal/scratch/ChronicPainDWI/outputs/qc/slurm-%A.out"
-
 module load StdEnv/2020 java/14.0.2 nextflow/21.10.3 apptainer/1.1.8
-
 
 my_main_nf='/home/ludoal/projects/def-pascalt-ab/ludoal/dev_scil/dmriqc_flow/main.nf'
 my_input='/home/ludoal/scratch/tpil_data/BIDS_longitudinal/tractoflow_results'  # depends on the profile option ...
